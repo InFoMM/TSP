d833a19d2b8aaaabe84353df4864f63ff6c05efb
Oliver Sheridan-Methven <oliver.sheridan-methven@maths.ox.ac.uk>
2016-12-05 16:11:16 +0000

    Template for how to rank the performance is given.

--- performance.m
+++ performance.m
@@ -7,3 +7,12 @@
 %     Displays the performance of various algorithms to find a path for the
 %     travelling salesman problem, usind the question data available from
 %     spain_example.m.
+
+[c, M] = spain_example();
+disp(sprintf('\n\nPerformance of:\n\t\tincreasing loop algorithm'))
+[p, d, t] = increasing_loop(M);
+disp(sprintf('Time taken:\n\t%g', t))
+disp(sprintf('Total distance:\n\t%g', d))
+disp(sprintf('Path taken:\n\t[%d, %d, %d, %d, %d, %d, %d, %d, %d, %d]', p))
+
+
