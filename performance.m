32820db3d5a80ca61f28d31ff55684dd4098f147
Oliver Sheridan-Methven <oliver.sheridan-methven@maths.ox.ac.uk>
2016-12-05 18:28:38 +0000

    Baseline performance indicated.

--- performance.m
+++ performance.m
@@ -7,6 +7,11 @@
 %     Displays the performance of various algorithms to find a path for the
 %     travelling salesman problem, usind the question data available from
 %     spain_example.m.
+% 
+% Baseline:
+% 
+%     The optimal route so far seems to take distance 2999km, and one such
+%     route with this performance is [1, 3, 5, 9, 7, 4, 8, 6, 2, 10].
 
 [c, M] = spain_example();
 disp(sprintf('\n\nPerformance of:\n\t\tincreasing loop algorithm'))
