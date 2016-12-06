c063fd54ece3c1b3f6c4e21f74e55148caeec189
Joseph Field <fieldj@maths.ox.ac.uk>
2016-12-06 10:56:45 +0000

    Added Joe Field greedy algorithms, including stochastic methods.

---
+++ greedy_algorithm_TSP.m
@@ -0,0 +1,81 @@
+%%
+% Author: Joseph Field 
+% Date:   December 2016.
+%
+% Description:
+%     Finds the shortest path to solve the travelling salesman problem,
+%     using a local greedy algorithm. For each node, the algorithm moves
+%     along the shortest edge, and removes all other connecting edges, to
+%     make sure that sub-loops do not occur. The final connection is then
+%     built once the final node is known.
+% Input: 
+%     A: Any chosen distance matrix.
+% Output:
+%     path_array: Optimal path, using this algorithm.
+%     cost: Total cost for for the chosen solution.
+
+%% 
+% The adjacency matrix for the Spain Assignment is below.
+%
+% A = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+%      515, 0, 0, 0, 0, 0, 0, 0, 0, 0
+%      353, 868, 0, 0, 0, 0, 0, 0, 0, 0
+%      422, 621, 434, 0, 0, 0, 0, 0, 0, 0
+%      482, 997, 129, 544, 0, 0, 0, 0, 0, 0
+%      673, 437, 841, 407, 951, 0, 0, 0, 0, 0
+%      634, 778, 631, 212, 756, 440, 0, 0, 0, 0
+%      815, 693, 827, 393, 937, 267, 363, 0, 0, 0
+%      609, 1046, 256, 538, 219, 945, 474, 837, 0, 0
+%      166, 349, 519, 352, 648, 501, 564, 673, 697, 0];
+%  
+%  A = A + A';
+
+%%
+function [path_array,cost] = greedy_algorithm_TSP(A)
+
+% Holder matrix to find the end connecting cost
+A_original  = A;
+
+% Check the size of the problem
+[~,N] = size(A);
+
+% Choose a random node to start with
+start_node = randperm(N,1);
+
+% Initialise the path
+path_array = zeros(N+1,1);
+
+path_array(1) = start_node;
+path_array(end) = start_node;
+
+% Initialise holders for nodes and current cost
+current_node = start_node;
+cost = 0;
+
+for t = 2:N
+    
+    i = current_node;
+
+    % Find the minimum cost node from this point, assuming we cannot return
+    % to a node already visited
+    index = find(A(i,:) ~= 0);
+    min_cost = min(A(i,index));
+    
+    % If there are multiple paths with the same cost, this condition
+    % chooses the first one in the (arbitrary) ordering
+    next_node = find(A(i,:) == min_cost);
+    next_node = min(next_node);
+    
+    cost = cost + min_cost;
+    
+    % This ensures that the point cannot be reached again
+    A(i,:) = 0;
+    A(:,i) = 0;
+    
+    current_node = next_node;
+    path_array(t) = current_node;
+    
+end
+
+path_array;
+cost = cost + A_original(path_array(end),path_array(end-1));
---
+++ stochastic_TSP.m
@@ -0,0 +1,87 @@
+%%
+% Author: Joseph Field
+% Date:   December 2016.
+%
+% Description:
+%     Finds the 'shortest' path for the travelling salesman problem, using
+%     a sequence of randomly generated paths. The final solution is the
+%     path which is shorter than the following 'n' paths.
+% Input: 
+%     A: Any chosen distance matrix.
+%     n: Number of 'wins' that a path must have before being chosen as the
+%     'shortest'.
+% Output:
+%     path_array: Optimal path, given numerically.
+%     cost: Total cost for for the chosen solution.
+%     time: Time taken to find the solution.
+
+%% 
+% The adjacency matrix for the Spain Assignment is below.
+%
+% A = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+%      515, 0, 0, 0, 0, 0, 0, 0, 0, 0
+%      353, 868, 0, 0, 0, 0, 0, 0, 0, 0
+%      422, 621, 434, 0, 0, 0, 0, 0, 0, 0
+%      482, 997, 129, 544, 0, 0, 0, 0, 0, 0
+%      673, 437, 841, 407, 951, 0, 0, 0, 0, 0
+%      634, 778, 631, 212, 756, 440, 0, 0, 0, 0
+%      815, 693, 827, 393, 937, 267, 363, 0, 0, 0
+%      609, 1046, 256, 538, 219, 945, 474, 837, 0, 0
+%      166, 349, 519, 352, 648, 501, 564, 673, 697, 0];
+%  
+%  A = A + A';
+
+%%
+function [path_array, cost, time] = stochastic_TSP(A, n)
+
+format compact;
+
+% Holder matrix to find the end connecting cost
+A_original  = A;
+
+% Check the size of the problem
+[~,N] = size(A);
+
+% Counter of unbeaten record
+winner_count = 0;
+
+% Stochastic path choice
+path_to_check = randperm(N,N);
+ptc = path_to_check;
+current_path = ptc;
+
+% Calculate the initial cost
+cost = 0;
+for j = 1:N-1
+    cost = cost + A(ptc(j),ptc(j+1));
+end
+cost = cost + A(ptc(end),ptc(1));
+
+% Start the timer after the initialisations
+tic;
+
+% Optimal choice must be unbeaten for 'n' trials
+while winner_count < n
+     
+    % Find a new path and then check against the current
+    ptc = randperm(N,N);
+    
+    % Calculate the initial cost
+    new_cost = 0;
+    for j = 1:N-1
+        new_cost = new_cost + A(ptc(j),ptc(j+1));
+    end
+    new_cost = new_cost + A(ptc(end),ptc(1));
+    
+    % If the new cost is cheaper, update the cost and the path
+    if new_cost < cost
+        current_path = ptc;
+        cost = new_cost;
+        winner_count = 0;
+    else
+        winner_count = winner_count + 1;
+    end
+end
+
+path_array = current_path;
+time = toc;
